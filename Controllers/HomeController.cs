fa43e2fd81e78b635d45478c3e1fff23976aa3d3
LeoKnox <LeoKnox@users.noreply.github.com>
2019-03-02 22:20:26 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FlashCard.Models;
+
+namespace FlashCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Partial = "Body";
+            return View();
+        }
+
+        [Route("change")]
+        [HttpGet]
+        public IActionResult change()
+        {
+            ViewBag.Partial = "Change";
+            ViewBag.Kanji = "&#19968;";
+            return View("Index");
+        }
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace FlashCard.Models
+{
+    public class MyModel
+    {
+       
+    }
+}
